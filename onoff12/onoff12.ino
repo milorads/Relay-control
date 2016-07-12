21f5d7df268fe50ce1cc88bde3e4fbf8dc3c9cf9
Milorad Sekulović <milorads@users.noreply.github.com>
2016-07-12 13:02:24 +0200

    initial commit

---
+++ onoff12/onoff12.ino
@@ -0,0 +1,60 @@
+//define relay pins 
+#define RELAY_PIN1 7
+#define RELAY_PIN2 8
+
+void setup()
+{
+	// initialize the pins for the relay as output
+  pinMode(RELAY_PIN1, OUTPUT);
+  pinMode(RELAY_PIN2, OUTPUT);
+  Serial.begin(9600); // open serial
+  Serial.println("Press 1/2 to toggle relay on/off");
+}
+
+void loop()
+{
+  static int relayVal1 = 0;
+  static int relayVal2 = 0;
+  int cmd;
+  //make loop when the serial is available
+  while (Serial.available() > 0)
+  {
+	// read the input all the time
+    cmd = Serial.read();
+      
+    switch (cmd)
+    {
+    case '1':
+      {
+        relayVal1 ^= 1; // xor current value with 1 (causes value to toggle)
+        if (relayVal1)
+          Serial.println("Relay n.1 on");
+        else
+          Serial.println("Relay n.1 off");
+        break;
+      }
+      case '2':
+      {
+       relayVal2 ^= 1; // xor current value with 1 (causes value to toggle)
+        if (relayVal2)
+          Serial.println("Relay n.2 on");
+        else
+          Serial.println("Relay n.2 off");
+        break; 
+      }
+    default:
+      {
+        Serial.println("Press the button 1 or 2 to toggle relay on/off");
+      }
+    }
+    
+    if (relayVal1)
+      digitalWrite(RELAY_PIN1, HIGH);
+    else
+      digitalWrite(RELAY_PIN1, LOW);
+      if (relayVal2)
+      digitalWrite(RELAY_PIN2, HIGH);
+    else
+      digitalWrite(RELAY_PIN2, LOW);
+   }
+}
---
+++ onoff12wifi/onoff12wifi.ino
@@ -0,0 +1,63 @@
+#include <Console.h>
+#define RELAY_PIN1 7
+#define RELAY_PIN2 8
+
+
+void setup()
+{
+  pinMode(RELAY_PIN1, OUTPUT);
+  pinMode(RELAY_PIN2, OUTPUT);
+  Bridge.begin();
+  Console.begin();
+  while(!Console);
+  digitalWrite(RELAY_PIN1, LOW);
+  digitalWrite(RELAY_PIN2, LOW);
+  //Console.println("Write 1 or 2 to toggle relay on/off");
+}
+
+void loop()
+{
+  static int relayVal1 = 0;
+  static int relayVal2 = 0;
+  int cmd;
+  
+  while (Console.available() > 0)
+  {
+    cmd = Console.read();
+      
+    switch (cmd)
+    {
+    case '1':
+      {
+        relayVal1 ^= 1;
+        if (!relayVal1)
+          Console.println("Prekidac broj 1 je ukljucen!");
+        else
+          Console.println("Prekidac broj 1 je iskljucen.");
+        break;
+      }
+      case '2':
+      {
+       relayVal2 ^= 1;
+        if (!relayVal2)
+          Console.println("Prekidac broj 2 je ukljucen!");
+        else
+          Console.println("Prekidac broj 2 je iskljucen.");
+        break; 
+      }
+    default:
+      {
+        Console.println("Doslo je do greske.");
+      }
+    }
+      
+    if (relayVal1)
+      digitalWrite(RELAY_PIN1, HIGH);
+    else
+      digitalWrite(RELAY_PIN1, LOW);
+      if (relayVal2)
+      digitalWrite(RELAY_PIN2, HIGH);
+    else
+      digitalWrite(RELAY_PIN2, LOW);
+   }
+}
